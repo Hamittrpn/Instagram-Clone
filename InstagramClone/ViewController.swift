70a244db07915bba843c6b665d6ef6df9db7e2b4
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-06 18:40:52 +0300

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Hamit  Tırpan on 6.10.2019.
+//  Copyright © 2019 Hamit  Tırpan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
