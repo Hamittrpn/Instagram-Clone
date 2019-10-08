9e79601f084fd1ddbda59b7ab8917513479adfc3
Hamit Tırpan <hamittirpan@gmail.com>
2019-10-08 09:47:59 +0300

    first commit

---
+++ InstagramClone/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  InstagramClone
+//
+//  Created by Hamit  Tırpan on 7.10.2019.
+//  Copyright © 2019 Hamit  Tırpan. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
